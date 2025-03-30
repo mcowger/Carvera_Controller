469790c5dcbfb74ac6850d19f6109b0e67b25ff3
Serge Bakharev <serge.bakharev@gmail.com>
2025-03-30 18:58:57 +1100

    Removed ignored ProbeSettingLabel class re-declaration

--- carveracontroller/addons/probing/ProbingControls.py
+++ carveracontroller/addons/probing/ProbingControls.py
@@ -2,12 +2,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
 from ...addons.tooltips.Tooltips import ToolTipButton
 
-class ProbeSettingLabel(GridLayout):
-    label_text = StringProperty("")
-    hint_text = StringProperty("")
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
 class ProbeButton(ToolTipButton):
     def __init__(self, image='', halign='center', valign='middle', **kwargs):
